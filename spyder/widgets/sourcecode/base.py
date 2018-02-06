1b5c36a32bd32829c9323f23eae55e011b60e5fa
Carlos Cordoba <ccordoba12@gmail.com>
2018-02-06 11:10:41 -0500

    Make mouse wheel event to do nothing

--- spyder/widgets/sourcecode/base.py
+++ spyder/widgets/sourcecode/base.py
@@ -1195,21 +1195,22 @@ class TextEditBaseWidget(QPlainTextEdit, BaseEditMixin):
     def wheelEvent(self, event):
         """Reimplemented to emit zoom in/out signals when Ctrl is pressed"""
         # This feature is disabled on MacOS, see Issue 1510
-        if sys.platform != 'darwin':
-            if event.modifiers() & Qt.ControlModifier:
-                if hasattr(event, 'angleDelta'):
-                    if event.angleDelta().y() < 0:
-                        self.zoom_out.emit()
-                    elif event.angleDelta().y() > 0:
-                        self.zoom_in.emit()
-                elif hasattr(event, 'delta'):
-                    if event.delta() < 0:
-                        self.zoom_out.emit()
-                    elif event.delta() > 0:
-                        self.zoom_in.emit()
-                return
-        QPlainTextEdit.wheelEvent(self, event)
-        self.highlight_current_cell()
+        #if sys.platform != 'darwin':
+        #    if event.modifiers() & Qt.ControlModifier:
+        #        if hasattr(event, 'angleDelta'):
+        #            if event.angleDelta().y() < 0:
+        #                self.zoom_out.emit()
+        #            elif event.angleDelta().y() > 0:
+        #                self.zoom_in.emit()
+        #        elif hasattr(event, 'delta'):
+        #            if event.delta() < 0:
+        #                self.zoom_out.emit()
+        #            elif event.delta() > 0:
+        #                self.zoom_in.emit()
+        #        return
+        #QPlainTextEdit.wheelEvent(self, event)
+        #self.highlight_current_cell()
+        pass
 
 
 class QtANSIEscapeCodeHandler(ANSIEscapeCodeHandler):
